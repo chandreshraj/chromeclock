233ce067d7306603575e8adb1ebd35ec3f6427cf
chandresh R <chandresh.r@comviva.com>
2020-12-27 21:22:37 +0530

    Updates

---
+++ precache-manifest.c777f0f400aa469c0569b264c888434c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42b6ac0d55b580a1b1323edb0012bbe7",
+    "url": "/chandreshraj/chromeclock/index.html"
+  },
+  {
+    "revision": "1a1356ab893a963dbc50",
+    "url": "/chandreshraj/chromeclock/static/css/main.b3280f44.chunk.css"
+  },
+  {
+    "revision": "e07eb11b6619d4a1a7bf",
+    "url": "/chandreshraj/chromeclock/static/js/2.36b53390.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chandreshraj/chromeclock/static/js/2.36b53390.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a1356ab893a963dbc50",
+    "url": "/chandreshraj/chromeclock/static/js/main.65baabf8.chunk.js"
+  },
+  {
+    "revision": "1d93f58761e86e3c89d5",
+    "url": "/chandreshraj/chromeclock/static/js/runtime-main.8c6e14a7.js"
+  }
+]);
